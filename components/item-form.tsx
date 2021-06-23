8a7ccc86e0915ed85bcce34885b33d99111b24f1
antenna-three <tkduc@outlook.com>
2021-06-24 03:58:40 +0900

    Change form to export state to query

--- components/item-form.tsx
+++ components/item-form.tsx
@@ -4,14 +4,15 @@ import _ from 'underscore'
 import { useRouter } from 'next/router'
 import SpinnerOverlay from './spinner-overlay'
 import ObjectiveFieldset from './objective-fieldset'
+import { useQueryObject } from '../lib/useQueryObject'
+import { getLargeCategory } from '../lib/get-large-category'
 
 export default function ItemForm({ itemList }: { itemList: {category: string, item: string}[] }) {
     const initialInputValues = Object.fromEntries(itemList.map(item => [item.item, '']))
     initialInputValues.objective = 'ap'
-    const [inputValues, setInputValues] = useState(initialInputValues)
+    const [inputValues, setInputValues] = useQueryObject(initialInputValues)
     const router = useRouter()
     const [isWaiting, setIsWaiting] = useState(false)
-    const [isOpen, setIsOpen] = useState(false)
 
     useEffect(() => {
         const savedInputValues = localStorage.getItem('inputValues')
@@ -24,7 +25,7 @@ export default function ItemForm({ itemList }: { itemList: {category: string, it
         event.preventDefault()
         setIsWaiting(true)
         router.push({
-            pathname: '/res',
+            pathname: '/query',
             query: inputValues
         })
     }
@@ -39,48 +40,42 @@ export default function ItemForm({ itemList }: { itemList: {category: string, it
     const itemGroups = _.groupBy(itemList, item => item.category)
     const categoryGroups = _.groupBy(
         Object.entries(itemGroups),
-        ([category, _]) => {
-            if (category.endsWith('素材')) {
-                return '強化素材'
-            } else if (category.endsWith('石')) {
-                return 'スキル石'
-            } else {
-                return 'モニュピ'
-            }
-        }
+        ([category, _]) => getLargeCategory(category)
     )
-    return (<>
-        <form onSubmit={handleSubmit}>
-            <ObjectiveFieldset
-                inputValues={inputValues}
-                handleChange={handleChange}
-            />
-            <div className="category-fieldsets">
-                {Object.entries(categoryGroups).map(( [largeCategory, categoryGroup] ) => (
-                    <details key={largeCategory} open={largeCategory=='強化素材'}>
-                        <summary>{largeCategory}</summary>
-                        <div className="item-fieldsets">
-                            {categoryGroup.map(([category, items]) => (
-                                <ItemFieldSet
-                                    key={category}
-                                    category={category}
-                                    items={items.map(item => item.item)}
-                                    inputValues={inputValues}
-                                    handleChange={handleChange}
-                                />
-                            ))}
-                        </div>
-                    </details>
-                ))}
-            </div>
-            <button type="submit">計算</button>
-            <style jsx>{`
-                .item-fieldsets {
-                    display: flex;
-                    flex-wrap: wrap;
-                }
-            `}</style>
-        </form>
-        {isWaiting && <SpinnerOverlay/>}</>
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <ObjectiveFieldset
+                    inputValues={inputValues}
+                    handleChange={handleChange}
+                />
+                <div className="category-fieldsets">
+                    {Object.entries(categoryGroups).map(( [largeCategory, categoryGroup] ) => (
+                        <details key={largeCategory} open={largeCategory=='強化素材'}>
+                            <summary>{largeCategory}</summary>
+                            <div className="item-fieldsets">
+                                {categoryGroup.map(([category, items]) => (
+                                    <ItemFieldSet
+                                        key={category}
+                                        category={category}
+                                        items={items.map(item => item.item)}
+                                        inputValues={inputValues}
+                                        handleChange={handleChange}
+                                    />
+                                ))}
+                            </div>
+                        </details>
+                    ))}
+                </div>
+                <button type="submit">計算</button>
+                <style jsx>{`
+                    .item-fieldsets {
+                        display: flex;
+                        flex-wrap: wrap;
+                    }
+                `}</style>
+            </form>
+            {isWaiting && <SpinnerOverlay/>}
+        </>
     )
 }
