314dac72d0f334faef9ad0355cb0caaa7100c9cd
antenna-three <tkduc@outlook.com>
2021-07-17 16:41:18 +0900

    Fix OGP title

--- components/head.tsx
+++ components/head.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 
 export default function MyHead ({title, children}: {title?: string, children?: React.ReactNode}) {
+    title = (title ? title + ' | ' : '') + 'FGO周回ソルバー'
     return (
         <Head>
-            <title>{title}{title ? ' | ' : ''}FGO周回ソルバー</title>
+            <title>{title}</title>
             <meta property="og:title" content={title}/>
             <meta property="og:image" content={`https://${process.env.VERCEL_URL}/_next/image?url=%2Fhermes.png&w=64&q=75`}/>
             <meta name="twitter:card" content="summary"/>
