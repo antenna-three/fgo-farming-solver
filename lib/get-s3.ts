7a88e25e199774dc5898976340ba194a72f5f638
antenna-three <tkduc@outlook.com>
2021-07-17 15:23:46 +0900

    Avoid collision of reserved environment variables

--- lib/get-s3.ts
+++ lib/get-s3.ts
@@ -1,42 +1,56 @@
-import S3 from 'aws-sdk/clients/s3'
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 import _ from 'underscore'
+import {Readable} from 'stream'
 
 export async function getS3(key: string): Promise<{[key: string]: string}[]> {
     const bucket = 'fgodrop'
 
-    const s3 = new S3()
+    const accessKeyId = process.env.MY_AWS_ACCESS_KEY_ID
+    const secretAccessKey = process.env.MY_AWS_SECRET_ACCESS_KEY
+    if (accessKeyId == null || secretAccessKey == null) {
+        console.log('Environment variables are not set')
+        return []
+    }
+
+    const client = new S3Client({ credentials: { accessKeyId, secretAccessKey } })
+    const command = new GetObjectCommand({ Bucket: bucket, Key: key })
 
     try {
-        const data = await s3.getObject({ Bucket: bucket, Key: key }).promise()
-        const body = data.Body?.toString()
-        if (body == null) {
-            return []
-        } else {
-            const lines = body.split(/\r\n/).filter(line => (line != ''))
-            const table = lines.map(line => line.split(','))
-            const header = table[0]
-            const en_header = header.map(h => {
-                switch (h) {
-                    case 'アイテム':
-                        return 'item' 
-                    case 'カテゴリ':
-                        return 'category'
-                    case 'エリア':
-                        return 'area'
-                    case 'クエスト名':
-                        return 'quest'
-                    case 'ドロップ率':
-                        return 'dropRate'
-                    default:
-                        return h
-                }
-            })
-            const rows = table.slice(1)
-            const obj = rows.map(
-                row => (Object.fromEntries(_.zip(en_header, row)))
-            )
-            return obj
+        const response = await client.send(command)
+        const body = response.Body as Readable
+        let data: string = ''
+        for await (const chunk of body) {
+            data += chunk
         }
+
+        if (body == null) {
+            console.log('Response body is empty')
+            return []
+        }
+        const lines = data.split(/\r\n/).filter(line => (line != ''))
+        const table = lines.map(line => line.split(','))
+        const header = table[0]
+        const en_header = header.map(h => {
+            switch (h) {
+                case 'アイテム':
+                    return 'item' 
+                case 'カテゴリ':
+                    return 'category'
+                case 'エリア':
+                    return 'area'
+                case 'クエスト名':
+                    return 'quest'
+                case 'ドロップ率':
+                    return 'dropRate'
+                default:
+                    return h
+            }
+        })
+        const rows = table.slice(1)
+        const obj = rows.map(
+            row => (Object.fromEntries(_.zip(en_header, row)))
+        )
+        return obj
     } catch (e) {
         console.log(e, e.stack)
         return []
