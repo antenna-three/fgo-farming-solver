ada3653c5889169ba865d742d3da84872308e8c8
antenna-three <tkduc@outlook.com>
2021-07-19 06:35:37 +0900

    Change create-tree to group by id

--- lib/create-tree.ts
+++ lib/create-tree.ts
@@ -4,14 +4,14 @@ import type {Node} from 'react-checkbox-tree'
 
 export function createTree(quests: {section: string, area: string, name: string, id: string}[]): {ids: string[], tree: Node[]} {
     const ids: string[] = []
-    const tree = Object.entries(_.groupBy(quests, ({section}) => (section)))
-        .map(([section, quests]) => {
-            const sectionId = quests[0].id[0]
+    const tree = Object.entries(_.groupBy(quests, ({id}) => (id[0])))
+        .map(([sectionId, quests]) => {
             ids.push(sectionId)
-            const subtree = Object.entries(_.groupBy(quests, ({area}) => (area)))
-                .map(([area, quests]) => {
-                    const areaId = quests[0].id.slice(0, 2)
+            const section = quests[0].section
+            const subtree = Object.entries(_.groupBy(quests, ({id}) => (id.slice(0, 2))))
+                .map(([areaId, quests]) => {
                     ids.push(areaId)
+                    const area = quests[0].area
                     const children = quests.map(({id, name}) => {
                         ids.push(id)
                         return {value: id, label: name}
