db12f9dca27468f3b5a0ddf1056b74c080a1da9a
antenna-three <tkduc@outlook.com>
2021-09-05 09:01:08 +0900

    Modify documents

--- lib/get-servants.ts
+++ lib/get-servants.ts
@@ -2,9 +2,9 @@ import { origin, region } from "../constants/atlasacademy"
 import { Servant } from "../interfaces"
 
 export const getServants = async () => {
-    const servantsUrl = `${origin}/export/${region}/basic_servant.json`
     const enumUrl = `${origin}/export/${region}/nice_enums.json`
     const classNames: string[] = await fetch(enumUrl).then(res => res.json()).then(obj => Object.values(obj.SvtClass))
+    const servantsUrl = `${origin}/export/${region}/basic_servant.json`
     const servants = fetch(servantsUrl)
         .then(res => res.json())
         .then((servants: Servant[]) => servants.filter(({ type }) => (type == 'normal' || type == 'heroine')))
