f1a5209c8f4c90094772c88c87045133240ac20e
antenna-three <tkduc@outlook.com>
2021-10-20 21:30:48 +0900

    Modify servant page title

--- components/servants/servant.tsx
+++ components/servants/servant.tsx
@@ -16,7 +16,7 @@ import { BreadcrumbLink } from '../common/breadcrumb-link'
 import { MaterialList } from './material-list'
 import { NiceServant, TargetKey } from '../../interfaces/atlas-academy'
 import { getJpClassName } from '../../lib/get-jp-class-name'
-import { Head } from '../common/head'
+import { Title } from '../common/title'
 
 const keys: { key: TargetKey; label: string }[] = [
   { key: 'ascension', label: '霊基再臨' },
@@ -29,35 +29,33 @@ export const Page = ({ servant }: { servant: NiceServant }) => {
   if (router.isFallback) {
     return <Text>読み込み中...</Text>
   }
+  const title = `${servant.name}の強化素材`
 
   return (
-    <>
-      <Head title={`${servant.name}の強化素材`} />
-      <VStack align="stretch" spacing={16}>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/servants">サーヴァント一覧</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem isCurrentPage>
-            <Text>{servant.name}</Text>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <VStack>
-          <HStack>
-            <Text color="yellow.500">{'★'.repeat(servant.rarity)}</Text>
-            <Text>{getJpClassName(servant.className)}</Text>
-          </HStack>
-          <Heading as="h1">{servant.name}</Heading>
-        </VStack>
-        <SimpleGrid minChildWidth="250px" spacing={8}>
-          {keys.map(({ key, label }) => (
-            <VStack align="stretch" key={key} spacing={4}>
-              <Heading size="lg">{label}素材</Heading>
-              <MaterialList materials={servant[`${key}Materials`]} />
-            </VStack>
-          ))}
-        </SimpleGrid>
+    <VStack align="stretch" spacing={16}>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/servants">サーヴァント一覧</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem isCurrentPage>
+          <Text>{title}</Text>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <VStack>
+        <HStack>
+          <Text color="yellow.500">{'★'.repeat(servant.rarity)}</Text>
+          <Text>{getJpClassName(servant.className)}</Text>
+        </HStack>
+        <Title>{title}</Title>
       </VStack>
-    </>
+      <SimpleGrid minChildWidth="250px" spacing={8}>
+        {keys.map(({ key, label }) => (
+          <VStack align="stretch" key={key} spacing={4}>
+            <Heading size="lg">{label}</Heading>
+            <MaterialList materials={servant[`${key}Materials`]} />
+          </VStack>
+        ))}
+      </SimpleGrid>
+    </VStack>
   )
 }
