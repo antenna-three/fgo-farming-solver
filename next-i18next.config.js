707b66e6c8e4379d73c0cb08db9ab4d79c4dc55a
antenna-three <tkduc@outlook.com>
2021-10-24 22:17:05 +0900

    Add locale path to config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
     i18n: {
         defaultLocale: 'ja',
         locales: ['en', 'ja'],
+        localePath: path.resolve('./public/locales')
     },
     localeExtension: 'yml',
     ns: ['common', 'farming', 'items', 'material', 'servants'],
