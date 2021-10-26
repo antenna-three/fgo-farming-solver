c41b019d0403127294b9f457267a0e5a580d63fb
antenna-three <tkduc@outlook.com>
2021-10-26 16:30:10 +0900

    Fix incompatible old result page

--- pages/farming/result.tsx
+++ pages/farming/result.tsx
@@ -1,13 +1,14 @@
-import { GetServerSidePropsContext } from 'next'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import { getDrops } from '../../lib/get-drops'
 import { Page } from '../../components/farming/result'
 import { getLocalItems } from '../../lib/get-local-items'
 import { getLocalQuests } from '../../lib/get-local-quests'
+import { ResultProps } from './results/[id]'
 
-export const getServerSideProps = async ({
+export const getServerSideProps: GetServerSideProps<ResultProps> = async ({
   query,
   locale,
-}: GetServerSidePropsContext) => {
+}) => {
   if (query == null) return { notFound: true }
   const _query = query as { items: string; quests: string; queries: string }
   const { items, quests, drop_rates } = await getDrops()
@@ -15,40 +16,41 @@ export const getServerSideProps = async ({
     getLocalItems(items, locale),
     getLocalQuests(quests, locale),
   ])
+  const localItemIndexes = Object.fromEntries(
+    localItems.map((item) => [item.id, item])
+  )
+  const localQuestIndexes = Object.fromEntries(
+    localQuests.map((quest) => [quest.id, quest])
+  )
   const props = {
     params: {
-      items: _query.queries
-        .split(',')
-        .map((p) => p.split(':'))
-        .map(([id, count]) => {
-          const { category, name } = items.find((i) => i.id == id) as {
-            [k: string]: string
-          }
-          return { id, category, name, count: parseInt(count) }
-        }),
+      items: Object.fromEntries(
+        _query.queries.split(',').map((p) => p.split(':'))
+      ),
+      quests: [],
+      objective: '',
     },
     items: _query.items
       .split(',')
       .map((i) => i.split(':'))
       .map(([id, count]) => {
-        const { category, name } = items.find((i) => i.id == id) as {
-          [k: string]: string
-        }
-        return { id, category, name, count: parseInt(count) }
+        const item = localItemIndexes[id]
+        return { ...item, count: parseInt(count) }
       }),
     quests: _query.quests
       .split(',')
       .map((q) => q.split(':'))
       .map(([id, lap]) => {
-        const { section, area, name, ap } = quests.find((q) => q.id == id) as {
-          ap: number
-          section: string
-          area: string
-          name: string
-        }
-        return { id, section, area, name, ap, lap: parseInt(lap) }
+        const quest = localQuestIndexes[id]
+        return { ...quest, lap: parseInt(lap) }
       }),
-    drop_rates,
+    drop_rates: drop_rates.map(({ item_id, quest_id, drop_rate_1 }) => ({
+      item_id: item_id,
+      item_name: localItemIndexes[item_id].name,
+      quest_id: quest_id,
+      quest_name: localQuestIndexes[quest_id].name,
+      drop_rate: drop_rate_1,
+    })),
   }
   const _props = {
     total_lap: props.quests.reduce((acc, { lap }) => acc + lap, 0),
@@ -57,9 +59,7 @@ export const getServerSideProps = async ({
 
   return {
     props: {
-      items: localItems,
-      quests: localQuests,
-      drop_rates,
+      ...props,
       ..._props,
     },
   }
