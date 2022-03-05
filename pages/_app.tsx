5735b45ebf9b8a46c39183c285eaa1b7ef89c6d5
antenna-three <tkduc@outlook.com>
2022-03-05 09:57:12 +0900

    Install NextAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../theme'
 import { Head } from '../components/common/head'
 import { Layout } from '../components/common/layout'
 import { useLanguage } from '../hooks/use-language'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => {
   useLanguage()
   return (
     <>
       <Head />
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </SessionProvider>
     </>
   )
 }
