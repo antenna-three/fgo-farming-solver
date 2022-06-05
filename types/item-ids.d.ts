b0fe2140752aa89f5bcb984021681938208e88f3
antenna-three <tkduc@outlook.com>
2022-06-05 14:36:30 +0900

    generate item id map

---
+++ types/item-ids.d.ts
@@ -0,0 +1,10 @@
+declare module '*/**/item-ids.json' {
+  interface ItemIds {
+    aaItemId: number
+    msItemId: number
+    apiItemId: string
+  }
+  const itemIds: ItemIds[]
+  export default itemIds
+}
+export {}
