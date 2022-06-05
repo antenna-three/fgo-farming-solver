615e73d369737114f47f756b05b083f84993b329
antenna-three <tkduc@outlook.com>
2022-06-05 14:35:42 +0900

    add script to generate id map

---
+++ lib/generate-item-ids.ts
@@ -0,0 +1,20 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { toMsItemId } from '../hooks/use-ms-item-id'
+import { Item } from '../interfaces/atlas-academy'
+import { toApiItemId } from './to-api-item-id'
+
+export const generateItemIds = async (items: Item[]) => {
+  const itemIds = items
+    .map((item) => ({
+      aaItemId: item.id,
+      msItemId: toMsItemId(item),
+      apiItemId: toApiItemId(item),
+    }))
+    .filter(({ msItemId, apiItemId }) => msItemId || apiItemId)
+  const json = JSON.stringify(itemIds, null, 4)
+  await fs.writeFile(
+    path.resolve(process.env.BASE_DIR ?? '', 'data/item-ids.json'),
+    json
+  )
+}
